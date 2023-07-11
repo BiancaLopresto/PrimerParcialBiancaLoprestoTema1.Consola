6a544316b088ed0a70673eaf34ea855d8b1d6f05
BiancaLopresto <bianlopre19@gmail.com>
2023-07-11 19:51:38 -0300

    Agregar archivos de proyecto.

---
+++ PrimerParcialBiancaLoprestoTema1.Consola/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace PrimerParcialBiancaLoprestoTema1.Consola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double altura;
+            double area;
+            double base1;
+            int contador;
+            double radio;
+            double volumen;
+            contador = 0;
+            do
+            {
+                Console.Write("Ingresar la Altura del Cilindro: ");
+                altura = Double.Parse(Console.ReadLine());
+                Console.Write("Ingrese el Radio del Cilindro: ");
+                radio = Double.Parse(Console.ReadLine());
+                if (altura != 0 && radio != 0)
+                {
+                    area = 2 * Math.PI * radio * (altura + radio);
+                    base1 = conseguirbase(radio);
+                    volumen = base1 * altura;
+                    Console.WriteLine("EL Area es: " + area);
+                    Console.WriteLine("El Volumen es: " + volumen);
+                }
+                else
+                {
+                    Console.WriteLine("No pueden ser ambos ceros");
+                }
+                contador = contador + 1;
+                Console.WriteLine("Los cilindros ingresados fueron: " + contador);
+            } while (!(altura == 0 && radio == 0));
+        }
+
+        private static double conseguirbase(double radio)
+        {
+            return Math.PI * Math.Pow(radio,2);
+        }
+    }
+}
+    
